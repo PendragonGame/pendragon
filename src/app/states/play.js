08ea672a1992350de60b1d6e4182890e747ee39a
Jacob Belewich <jacobbelewich@gmail.com>
2017-10-05 14:18:29 -0400

    .

--- src/app/states/play.js
+++ src/app/states/play.js
@@ -155,8 +155,9 @@ Play.update = function() {
     // Displays the hitbox for the Player
     // this.game.debug.body(this.player);
     this.monster.gotoXY(this.player.x+this.player.body.width /
-        2 + this.player.body.offset.x - 32, this.player.y+this.player.body.height /
-        2 + this.player.body.offset.y + 16, this.navMesh);
+        2 + this.player.body.offset.x - 32,
+         this.player.y+this.player.body.height /
+          2 + this.player.body.offset.y + 16, this.navMesh);
     // SHIFT for running
     let sprint = false;
     if (this.keyboard.isDown(Phaser.Keyboard.SHIFT)) {
