b6b65b562931c8f96e8ef0934013896abfdd8307
Jacob Belewich <jacobbelewich@gmail.com>
2017-10-09 04:28:03 -0400

    even cooler menu

--- src/app/states/menu.js
+++ src/app/states/menu.js
@@ -100,7 +100,7 @@ Menu.update = function() {
     this.r = game.rnd.integerInRange(0, 256);
     this.g = game.rnd.integerInRange(0, 256);
     this.b = game.rnd.integerInRange(0, 256);    
-    this.pendragonText.setShadow(5, 5, 'rgba('+ this.r +','+this.b +','+this.g +',0.5)', 10);
+    this.pendragonText.setShadow(5, 5, 'rgba('+ this.r +','+this.b +','+this.g +',.75)', 1);
 
 };
 
