65023b16fc34dcb43b474fb6aea8b9e06b966212
Jacob Belewich <jacobbelewich@gmail.com>
2017-10-19 05:07:27 -0400

    refactor menu ui

--- src/app/states/gameover.js
+++ src/app/states/gameover.js
@@ -1,42 +1,5 @@
+const ui = require('../ui/ui');
 let GameOver = {};
-/**
- * 
- * @param {*} x 
- * @param {*} y 
- * @param {*} text 
- * @param {*} func 
- */
-function MenuButton(x, y, text, func) {
-    // add text over the button
-    this.text = game.add.text(x,
-        y, text);
-    this.text.anchor.setTo(.5, .5);
-    this.text.font = 'Press Start 2P';
-    this.text.fill = '#FFFFFF';
-    this.text.fontSize = '35pt';
-    // the button
-    this.button = game.add.button(
-        x, y, null, func, this, 2, 1, 0);
-    this.button.width = this.text.width;
-    this.button.height = this.text.height;
-    // hover effect
-    this.button.anchor.setTo(.5, .5);
-    this.button.onInputOver.add(function() {
-        this.text.setShadow(3, 3, 'rgba(256,256,256,0.5)', 5);
-    }, this);
-    // hover off effect
-    this.button.onInputOut.add(function() {
-        this.text.setShadow(0, 0, 'rgba(0,0,0,0.5)', 0);
-        this.text.fill = '#FFFFFF';
-    }, this);
-    this.button.fixedToCamera = true;
-    this.text.fixedToCamera = true;
-
-    this.kill = function() {
-        this.button.kill();
-        this.text.kill();
-    };
-}
 
 GameOver.create = function() {
     let gameoverText = game.add.text(game.camera.width/2, 100, 'Game Over');
@@ -45,20 +8,34 @@ GameOver.create = function() {
     gameoverText.fill = '#FFFFFF';
     gameoverText.fontSize = '35pt';
 
-    let scoreText = game.add.text(game.camera.width/2, 200, 'Score:'+game.score);
+    let scoreText = game.add.text(game.camera.width/2,
+         200, 'Score:'+game.score);
     scoreText.anchor.setTo(.5, .5);
     scoreText.font = 'Press Start 2P';
     scoreText.fill = '#FFFFFF';
     scoreText.fontSize = '25pt';
 
-    let dayText = game.add.text(game.camera.width/2, 260, 'Days Survived:'+game.dayCount);
+    let dayText = game.add.text(game.camera.width/2,
+         260, 'Days Survived:'+game.dayCount);
     dayText.anchor.setTo(.5, .5);
     dayText.font = 'Press Start 2P';
     dayText.fill = '#FFFFFF';
     dayText.fontSize = '25pt';
 
-    new MenuButton(game.camera.width/2, 340, 'Main Menu', ()=> {
-        game.state.start('Menu');
-    });
+    let menu = new ui.MenuButton(game.camera.width/2, game.camera.height/2+60,
+         'Main Menu', null, ()=>{
+             game.state.start('Menu');
+            }, '35pt');
+    menu.text.fill = '#FFFFFF';
+     // hover effect
+     menu.button.onInputOver.add(function() {
+        //  menu.text.setShadow(3, 3, 'rgba(256,256,256,.75)', 5);
+        menu.text.fontSize = '38pt';
+     }, this);
+     // hover off effect
+     menu.button.onInputOut.add(function() {
+        //  menu.text.setShadow(0, 0, 'rgba(0,0,0,0.5)', 0);
+        menu.text.fontSize = '35pt';
+     }, this);
 };
 module.exports = GameOver;
--- src/app/states/load-menu.js
+++ src/app/states/load-menu.js
@@ -1,5 +1,6 @@
 const dataStore = require('../util/data');
 const moment = require('moment');
+const ui = require('../ui/ui');
 
 let LoadMenu = {};
 
@@ -16,131 +17,6 @@ function loadGame() {
         });
 }
 
-/**
- * 
- * @param {*} x 
- * @param {*} y 
- * @param {*} text 
- * @param {*} key
- * @param {*} func
- */
-function MenuButton(x, y, text, key, func) {
-    // add text over the button
-    this.key = key;
-    this.text = game.add.text(x,
-        y, text);
-    this.text.anchor.setTo(.5, .5);
-    this.text.font = 'Press Start 2P';
-    this.text.fill = '#000000';
-    this.text.fontSize = '16pt';
-    // the button
-    this.button = game.add.button(
-        x, y, null, func, this, 2, 1, 0);
-    this.button.width = this.text.width;
-    this.button.height = this.text.height;
-    // hover effect
-    this.button.anchor.setTo(.5, .5);
-    this.button.onInputOver.add(function() {
-        this.text.setShadow(3, 3, 'rgba(0,0,0,0.5)', 5);
-    }, this);
-    // hover off effect
-    this.button.onInputOut.add(function() {
-        this.text.setShadow(0, 0, 'rgba(0,0,0,0.5)', 0);
-        this.text.fill = '#111111';
-    }, this);
-    this.button.fixedToCamera = true;
-    this.text.fixedToCamera = true;
-
-    this.kill = function() {
-        this.button.kill();
-        this.text.kill();
-    };
-}
-/**
- * 
- * @param {*} saves  array of timestamps 
- */
-function ButtonList(saves) {
-    this.saveButtons = [];
-    this.saves = saves;
-    this.currentPage = 0;
-    let autosaveIndex = saves.findIndex((s) => s.key === 'autosave');
-    if (autosaveIndex > -1) {
-        saves.splice(autosaveIndex, 1);
-        this.autosave = new MenuButton(game.camera.width / 2,
-            100, 'Autosave',
-            'autosave',
-            loadGame
-        );
-    }
-    let currentH = 160;
-    this.startI = 0;
-    this.currentI = 0;
-    if (saves) {
-        for (let i = this.currentI; i < saves.length; i++) {
-            this.currentI = i;
-            this.saveButtons.push(new MenuButton(game.camera.width / 2,
-                currentH,
-                saves[i].title,
-                saves[i].key,
-                loadGame));
-            currentH += 60;
-            if (currentH > 460) {
-                break;
-            }
-        }
-    }
-    console.log(this.startI, this.currentI);
-    this.nextPageButton = new MenuButton(
-        game.camera.width / 2 + 80,
-        game.camera.height - 80, '>', () => {
-            if (saves.length - this.startI < 6) return;
-            this.currentPage += 1;
-            for (; this.startI <= this.currentI; this.startI++) {
-                this.saveButtons[this.startI].kill();
-            }
-            let currentH = 160;
-            for (let i = this.startI; i < saves.length; i++) {
-                this.currentI = i;
-                this.saveButtons[i] = (new MenuButton(game.camera.width / 2,
-                    currentH,
-                    saves[i].title,
-                    saves[i].key,
-                    loadGame));
-                currentH += 60;
-                if (currentH > 460) {
-                    break;
-                }
-            }
-        });
-
-    this.nextPrevPage = new MenuButton(
-        game.camera.width / 2 - 80,
-        game.camera.height - 80, '<', () => {
-            if (this.currentPage === 0) return;
-            this.currentPage -= 1;
-            for (let i = this.startI; i <= this.currentI; i++) {
-                this.saveButtons[i].kill();
-            }
-            this.startI -= 6;
-            this.currentI = this.startI;
-            currentH = 160;
-            for (let i = this.startI; i < saves.length; i++) {
-                this.currentI = i;
-                this.saveButtons[i] = (new MenuButton(game.camera.width / 2,
-                    currentH,
-                    saves[i].title,
-                    saves[i].key,
-                    loadGame));
-                currentH += 60;
-                if (currentH > 460) {
-                    break;
-                }
-            }
-        });
-}
-
-
 LoadMenu.preload = function() {
 
 };
@@ -153,37 +29,13 @@ LoadMenu.create = function() {
     this.bgLayer = this.map.createLayer('bg1');
     game.camera.x = game.menuCameraPos;
 
-    // back button to go to menu screen
-    // add a back button
-    this.back = game.add.button(game.camera.width - 100,
-        game.camera.height - 80,
-        null,
-        function() {
+    // // back button to go to menu screen
+    // // add a back button
+    this.back = new ui.MenuButton(game.camera.width - 100,
+         game.camera.height - 80, 'Back', null, ()=>{
             // when pressed start loading the game
             game.state.start('Menu');
-        }, this, 2, 1, 0);
-    // position the button in the right spot
-    this.back.width = 130;
-    this.back.height = 50;
-    this.back.x -= 120 / 2;
-    // add text over the button
-    this.backText = game.add.text(game.camera.width - 80 * 2,
-        game.camera.height - 85, 'Back');
-    this.backText.font = 'Press Start 2P';
-    this.backText.fill = '#000000';
-    this.backText.fontSize = '25pt';
-    // hover effect
-    this.back.onInputOver.add(function() {
-        this.backText.setShadow(3, 3, 'rgba(0,0,0,0.5)', 5);
-    }, this);
-    // hover off effect
-    this.back.onInputOut.add(function() {
-        this.backText.setShadow(0, 0, 'rgba(0,0,0,0.5)', 0);
-        this.backText.fill = '#111111';
-    }, this);
-    this.back.fixedToCamera = true;
-    this.backText.fixedToCamera = true;
-
+    }, '30pt');
 
     let saveStates = [];
     dataStore.getSaveStates()
@@ -208,7 +60,17 @@ LoadMenu.create = function() {
                 if (am.isBefore(bm)) return 1;
                 return 0;
             });
-            this.buttonList = new ButtonList(saveStates);
+            this.buttonList = new ui.ButtonList(saveStates, loadGame);
+            let autosaveIndex = saveStates.findIndex(
+                (s) =>s.key === 'autosave');
+            if (autosaveIndex > -1) {
+                saveStates.splice(autosaveIndex, 1);
+                this.autosave = new ui.MenuButton(game.camera.width / 2,
+                    100, 'Autosave',
+                    'autosave',
+                    loadGame
+                );
+            }
         })
         .catch((reason) => {
             console.error(reason);
