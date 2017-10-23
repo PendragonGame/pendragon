d16b7a5c389ca7d85066648a91eb12f35ccce8e5
mike4tin <msfortin@buffalo.edu>
2017-10-23 17:26:33 -0400

    Added comments, changed when music stops

--- src/app/states/play.js
+++ src/app/states/play.js
@@ -189,6 +189,11 @@ Play.create = function() {
     this.map.setCollisionBetween(1, 10000, true, this.blockLayer);
     this.map.setCollisionBetween(1, 10000, true, this.blockOverlap);
 
+    /*Starts music when the player hits the play button.
+    The music will loop indefinitely and I adjusted the volume
+    down a little bit from the original
+    Music stops when the player dies and the gameover screen pops up
+    Mike Fortin*/
     this.mainMusic = game.add.audio('main_music');
     this.mainMusic.loop = true;
     this.mainMusic.volume = .40;
@@ -311,11 +316,11 @@ Play.create = function() {
 
 Play.update = function() {
     if (this.player.state === 'dead') {
-        this.mainMusic.pause();
         game.score = this.player.score;
         game.dayCount = this.player.daysSurvived;
         setTimeout(() => {
             game.state.start('Game Over');
+            this.mainMusic.pause(); //Pauses the main music at the gameover screen
         }, 2000);
     }
     while (this.fullHealthBar.width < 146) this.fullHealthBar.width += 1;
